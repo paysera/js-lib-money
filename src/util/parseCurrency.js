fa002feacb01312beb0fa17347c14f0ae5d9bdf6
Vadim Saveljev <v.saveljev@paysera.lt>
2019-01-08 16:52:50 +0200

    require currency, otherwise through error

--- src/util/parseCurrency.js
+++ src/util/parseCurrency.js
@@ -3,6 +3,9 @@ import { UnsupportedCurrencyError } from '../Error';
 import fractions from '../fractions';
 
 export default (currency) => {
+    if (currency === null || currency === '') {
+        throw new UnsupportedCurrencyError(`Currency not provided`);
+    }
     const upperCaseCurrency = currency.toUpperCase();
 
     if (isUndefined(fractions[upperCaseCurrency])) {
